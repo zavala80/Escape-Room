cdb6c31917e22aa6c14332693770f50ce52b95af
Calixto Zavala <zavala7980@gmail.com>
2022-02-11 20:15:35 -0600

    El personaje ahora está limitado a no caminar fuera del nivel

--- componentes/player/player.gd
+++ componentes/player/player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var motion = Vector2(0, 0) # Motion inicial del personaje
 const VELOCIDAD = 250 # Velocidad del personaje
+var puede_caminar = true
+var tamanio_pantalla = get_viewport_rect().size
 
 func _ready():
 	set_process(true)
@@ -17,6 +19,12 @@ func administrar_inputs():
 	# Movimiento horizontal y vertical
 	motion.x = (int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))) * VELOCIDAD
 	motion.y = (int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))) * VELOCIDAD
+	
+	# Limitamos el espacio donde se puede mover el personaje
+	if self.position.x < 0:
+		self.position.x = 0
+	elif self.position.x > 1024:
+		self.position.x = 1024
 	
 	# Reinicio del nivel
 	if Input.is_action_just_released("ui_restart"):
